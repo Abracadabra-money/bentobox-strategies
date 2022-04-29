f5817a90e9ef40d263cb078d240277ca9abdc386
0xCalibur <0xCalibur@protonmail.com>
2022-04-29 14:59:54 -0500

    new cakepool strat draft

--- contracts/strategies/CakeStrategyV2.sol
+++ contracts/strategies/CakeStrategyV2.sol
@@ -4,16 +4,17 @@ pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../BaseStrategy.sol";
+import "../interfaces/pancakeswap/ICakePool.sol";
 import "../interfaces/ISushiSwap.sol";
-import "../interfaces/IMasterChef.sol";
 import "../libraries/Babylonian.sol";
 
 contract CakeStrategyV2 is BaseStrategy {
     using SafeERC20 for IERC20;
 
     event LpMinted(uint256 total, uint256 strategyAmount, uint256 feeAmount);
-    ICakeChef private immutable masterchef;
+    ICakePool private immutable cakePool;
 
+    uint256 public depositAmount;
     uint256 public fee; // fees on rewards
     address public feeCollector;
 
@@ -29,37 +30,47 @@ contract CakeStrategyV2 is BaseStrategy {
         address _factory,
         address _bridgeToken,
         address _strategyExecutor,
-        ICakeChef _masterchef,
+        ICakePool _cakePool,
         bytes32 _pairCodeHash
     ) BaseStrategy(_strategyToken, _bentoBox, _factory, _bridgeToken, _strategyExecutor, _pairCodeHash) {
-        masterchef = _masterchef;
+        cakePool = _cakePool;
         feeCollector = _msgSender();
         fee = 10;
 
-        IERC20(_strategyToken).safeApprove(address(_masterchef), type(uint256).max);
+        IERC20(_strategyToken).safeApprove(address(_cakePool), type(uint256).max);
     }
 
     function _skim(uint256 amount) internal override {
-        masterchef.enterStaking(amount);
+        cakePool.deposit(amount, 0);
+        (depositAmount, , , , , , , , ) = cakePool.userInfo(address(this));
     }
 
     function _harvest(uint256) internal override returns (int256) {
-        masterchef.leaveStaking(0);
+        /// @dev CakePool auto compound reward and increase share price in CAKE.
+        /// Withdraw the accumulated amount instead.
+        uint256 withdrawFee = cakePool.calculateWithdrawFee(address(this), depositAmount);
+        uint256 currentAmount = cakePool.getPricePerFullShare() * depositAmount;
+        require(currentAmount - withdrawFee >= depositAmount, "not profitable");
 
+        // only withdraw the rewards
+        cakePool.withdrawByAmount(currentAmount - depositAmount);
         uint256 total = IERC20(strategyToken).balanceOf(address(this));
         uint256 feeAmount = (total * fee) / 100;
 
         IERC20(strategyToken).safeTransfer(feeCollector, feeAmount);
 
+        (depositAmount, , , , , , , , ) = cakePool.userInfo(address(this));
         return int256(0);
     }
 
     function _withdraw(uint256 amount) internal override {
-        masterchef.leaveStaking(amount);
+        cakePool.withdraw(amount);
+        (depositAmount, , , , , , , , ) = cakePool.userInfo(address(this));
     }
 
     function _exit() internal override {
-        masterchef.emergencyWithdraw(0);
+        cakePool.withdrawAll();
+        depositAmount = 0;
     }
 
     function setFeeCollector(address _feeCollector, uint256 _fee) external onlyOwner {
--- deploy/CakeStrategy.ts
+++ deploy/CakeStrategy.ts
@@ -3,17 +3,17 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { network, ethers } from "hardhat";
 import { CakeStrategy } from "../typechain";
 import { wrappedDeploy } from "../utilities";
-import { xMerlin } from "../test/constants";
+import { Constants, xMerlin } from "../test/constants";
 
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts } = hre;
   const { deployer } = await getNamedAccounts();
 
   const strategyToken = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"; // CAKE
-  const degenBox = "0x090185f2135308bad17527004364ebcc2d37e5f6";
+  const degenBox = Constants.bsc.degenBox;
   const factory = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"; // Cake Factory
   const bridgeToken = ethers.constants.AddressZero;
-  const masterChef = "0x45c54210128a065de780C4B0Df3d16664f7f859e"; // CakePool (New Staking Contract)
+  const masterChef = Constants.bsc.pancakeSwap.cakePool;
   const pairHashCode = "0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5"; // pair hash code for Pancake
 
   const CakeStrategy = await wrappedDeploy<CakeStrategy>("CakeStrategyV2", {
