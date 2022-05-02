dd85eeadd21443491260337ccc28a4ce7e53abbd
0xCalibur <0xCalibur@protonmail.com>
2022-05-02 10:08:03 -0500

    merge conflicts

--- test/constants.ts
+++ test/constants.ts
@@ -3,24 +3,76 @@ export const xCalibur = "0xfB3485c2e209A5cfBDC1447674256578f1A80eE3";
 export const PopsicleMultisig = "0x7dc4eA80fE36788f393b5705909F7f0aAc305a86";
 
 export const Constants = {
-    bsc: {
-        degenBox: "0x090185f2135308bad17527004364ebcc2d37e5f6",
-        pancakeSwap: {
-            cake: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
-            cakePool: "0x45c54210128a065de780C4B0Df3d16664f7f859e",
-        }
+  bsc: {
+    degenBox: "0x090185f2135308bad17527004364ebcc2d37e5f6",
+    pancakeSwap: {
+      cake: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
+      cakePool: "0x45c54210128a065de780C4B0Df3d16664f7f859e",
     },
-    fantom: {
-        degenBox: "0x74A0BcA2eeEdf8883cb91E37e9ff49430f20a616",
-        degenBoxOwner: "0x3d995ECE005E9789C22acB1e359Ff615FbdD96ba",
-        mim: "0x82f0B8B456c1A451378467398982d4834b6829c1",
-        wftm: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
-        spîrit: "0x5Cc61A78F164885776AA610fb0FE1257df78E59B",
-        spiritFtmMimPair: "0xB32b31DfAfbD53E310390F641C7119b5B9Ea0488",
-        spiritSwapInitHash: "0xe242e798f6cee26a9cb0bbf24653bf066e5356ffeac160907fe2cc108e238617",
-        spiritFtmMimGauge: "0x0B905475bEa057060D066f3D1F85E6902Ae62557",
-        spiritGaugeProxy: "0x420b17f69618610DE18caCd1499460EFb29e1d8f",
-        spiritRouter: "0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52",
-        spiritFactory: "0xEF45d134b73241eDa7703fa787148D9C9F4950b0",
-    }
-}
+  },
+  fantom: {
+    degenBox: "0x74A0BcA2eeEdf8883cb91E37e9ff49430f20a616",
+    degenBoxOwner: "0x3d995ECE005E9789C22acB1e359Ff615FbdD96ba",
+    mim: "0x82f0B8B456c1A451378467398982d4834b6829c1",
+    wftm: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+    spîrit: "0x5Cc61A78F164885776AA610fb0FE1257df78E59B",
+    spiritFtmMimPair: "0xB32b31DfAfbD53E310390F641C7119b5B9Ea0488",
+    spiritSwapInitHash: "0xe242e798f6cee26a9cb0bbf24653bf066e5356ffeac160907fe2cc108e238617",
+    spiritFtmMimGauge: "0x0B905475bEa057060D066f3D1F85E6902Ae62557",
+    spiritGaugeProxy: "0x420b17f69618610DE18caCd1499460EFb29e1d8f",
+    spiritRouter: "0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52",
+    spiritFactory: "0xEF45d134b73241eDa7703fa787148D9C9F4950b0",
+  },
+  mainnet: {
+    degenBox: "0xd96f48665a1410C0cd669A88898ecA36B9Fc2cce",
+    multiSig: "0x5f0DeE98360d8200b20812e174d139A1a633EDd2",
+    mim: "0x99D8a9C45b2ecA8864373A26D1459e3Dff1e17F3",
+    spell: "0x090185f2135308BaD17527004364eBcC2D37e5F6",
+    ust: "0xa47c8bf37f92aBed4A126BDA807A7b7498661acD",
+    usdc: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    usdt: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    stargate: {
+      stg: "0xAf5191B0De278C7286d6C7CC6ab6BB8A73bA2Cd6",
+      router: "0x8731d54E9D02c286767d56ac03e8037C07e01e98",
+      usdcPool: "0xdf0770dF86a8034b3EFEf0A1Bb3c889B8332FF56",
+      usdtPool: "0x38EA452219524Bb87e18dE1C24D3bB59510BD783",
+    },
+    curve: {
+      mim3Crv: "0x5a6A4D54456819380173272A5E8E9B9904BdF41B",
+    },
+  },
+  arbitrum: {
+    degenBox: "0x74c764D41B77DBbb4fe771daB1939B00b146894A",
+    mim: "0xFEa7a6a0B346362BF88A9e4A88416B77a57D6c2A",
+    usdc: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+    usdt: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+    stargate: {
+      stg: "0x6694340fc020c5E6B96567843da2df01b2CE1eb6",
+      router: "0x53Bf833A5d6c4ddA888F69c22C88C9f356a41614",
+      usdcPool: "0x892785f33CdeE22A30AEF750F285E18c18040c3e",
+      usdtPool: "0xB6CfcF89a7B22988bfC96632aC2A9D6daB60d641",
+    },
+    curve: {
+      mim2crvPool: "0x30dF229cefa463e991e29D42DB0bae2e122B2AC7",
+    },
+  },
+  avalanche: {
+    degenBox: "0x1fC83f75499b7620d53757f0b01E2ae626aAE530",
+    mim: "0x130966628846BFd36ff31a822705796e8cb8C18D",
+    spell: "0xCE1bFFBD5374Dac86a2893119683F4911a2F7814",
+    ust: "0xb599c3590F42f8F995ECfa0f85D2980B76862fc1",
+    usdt: "0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7",
+    usdte: "0xc7198437980c041c805A1EDcbA50c1Ce5db95118",
+    usdc: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+    usdce: "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664",
+    platypus: {
+      router: "0x73256EC7575D999C360c1EeC118ECbEFd8DA7D12",
+    },
+    stargate: {
+      stg: "0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590",
+      router: "0x45A01E4e04F14f7A4a6702c74187c5F6222033cd",
+      usdcPool: "0x1205f31718499dBf1fCa446663B532Ef87481fe1",
+      usdtPool: "0x29e38769f23701A2e4A8Ef0492e19dA4604Be62c",
+    },
+  },
+};
